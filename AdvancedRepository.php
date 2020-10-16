76a219de3d8d00cd75e5769a97ca245ff876c0e7
Skrip42 <Skrip42@gmail.com>
2020-10-16 12:03:47 +0700

    Update AdvancedRepository.php

--- AdvancedRepository.php
+++ AdvancedRepository.php
@@ -13,30 +13,125 @@ abstract class AdvancedRepository extends ServiceEntityRepository
     {
         $query = $this->createQueryBuilder('base');
         foreach ($params as $key => $value) {
-            $parts = array_merge(
-                ['base'],
-                explode('.', $key)
-            );
-            $fullKey = implode('_', $parts);
-            if (count($parts) == 2) {
-                $query->andWhere($parts[0] . '.' . $parts[1] . ' = :' . $fullKey);
-            } else {
-                for ($i = 0; $i < count($parts) - 3; $i++) {
+            $valKey = $this->generateKey($key);
+            $parts = $this->prepareParam($key, $valKey);
+            $i = 0;
+            for (; $i < count($parts) - 1; $i++) {
+                if (count($parts[$i]) == 2) {
+                    $query->innerJoin($parts[$i][0], $parts[$i][1]);
+                } elseif (count($parts[$i]) === 3) {
                     $query->innerJoin(
-                        $parts[$i] . '.' . $parts[$i + 1],
-                        $parts[$i + 1]
+                        $parts[$i][0],
+                        $parts[$i][1],
+                        Expr\Join::WITH,
+                        $parts[$i][2]
+                    );
+                } else {
+                    $cond = array_slice($parts[$i], 2);
+                    $and = $query->expr()->andX(...$cond);
+                    $query->innerJoin(
+                        $parts[$i][0],
+                        $parts[$i][1],
+                        Expr\Join::WITH,
+                        $and
                     );
                 }
-                $query->innerJoin(
-                    $parts[$i] . '.' . $parts[$i + 1],
-                    $parts[$i + 1],
-                    Expr\Join::WITH,
-                    $parts[$i + 1] . '.' . $parts[$i + 2] . '= :' . $fullKey
-                );
             }
-            $query->setParameter($fullKey, $value);
+            $value = explode('::', $value, 2);
+            if (count($value) == 1) {
+                $query->andWhere($parts[$i][0] . ' = :' . $parts[$i][1]);
+                $query->setParameter($valKey, $value);
+            } else {
+                switch ($value[0]) {
+                    case 'not':
+                        $query->andWhere($parts[$i][0] . ' != :' . $parts[$i][1]);
+                        $query->setParameter($valKey, '%' . $value[1] . '%');
+                        break;
+                    case 'like':
+                        $query->andWhere($parts[$i][0] . ' like :' . $parts[$i][1]);
+                        $query->setParameter($valKey, '%' . $value[1] . '%');
+                        break;
+                    case 'notLike':
+                        $query->andWhere($parts[$i][0] . ' like :' . $parts[$i][1]);
+                        $query->setParameter($valKey, $value[1]);
+                        break;
+                    case 'in':
+                        $query->andWhere($parts[$i][0] . ' in (:' . $parts[$i][1] . ')');
+                        $query->setParameter($valKey, explode(',', $value[1]));
+                        break;
+                    case 'notIn':
+                        $query->andWhere($parts[$i][0] . ' not in (:' . $parts[$i][1] . ')');
+                        $query->setParameter($valKey, explode(',', $value[1]));
+                        break;
+                    case 'less':
+                        $query->andWhere($parts[$i][0] . ' < :' . $parts[$i][1]);
+                        $query->setParameter($valKey, $value[1]);
+                        break;
+                    case 'lessOrEq':
+                        $query->andWhere($parts[$i][0] . ' <= :' . $parts[$i][1]);
+                        $query->setParameter($valKey, $value[1]);
+                        break;
+                    case 'more':
+                        $query->andWhere($parts[$i][0] . ' > :' . $parts[$i][1]);
+                        $query->setParameter($valKey, $value[1]);
+                        break;
+                    case 'moreOrEq':
+                        $query->andWhere($parts[$i][0] . ' >= :' . $parts[$i][1]);
+                        $query->setParameter($valKey, $value[1]);
+                        break;
+                    case 'empty':
+                        $query->andWhere($parts[$i][0] . ' is null');
+                        break;
+                    case 'notEmpty':
+                        $query->andWhere($parts[$i][0] . ' is not null');
+                        break;
+                }
+            }
         }
         return $query;
+    }
+
+
+
+    private function generateKey($params)
+    {
+        $params = str_replace('.', '_', $params);
+        $params = str_replace(':', '_', $params);
+        $params = str_replace('\\', '', $params);
+        return $params;
+    }
+
+    private function prepareParam($params, $valKey)
+    {
+        if (empty($params)) {
+            return [];
+        }
+        $chain = [];
+        $parts = array_merge(
+            ['base'],
+            explode('.', $params)
+        );
+        $pp = $parts[0];
+        $i = 1;
+        for (; $i < count($parts) - 1; $i++) {
+            $tmp = [];
+            $sparts = explode(':', $parts[$i]);
+            if (strpos($sparts[0], '\\') !== false) {
+                $tmp[] = $sparts[0];
+            } else {
+                $tmp[] = $pp . '.' . $sparts[0];
+            }
+            $alias = str_replace('\\', '', $sparts[0]);
+            $tmp[] = $alias;
+            for ($j = 1; $j < count($sparts); $j += 2) {
+                $tmp[] = $alias . '.' . $sparts[$j]
+                    . ' = '  . $pp . '.' . $sparts[$j + 1];
+            }
+            $pp = $alias;
+            $chain[] = $tmp;
+        }
+        $chain[] = [$pp . '.' . $parts[$i], $valKey];
+        return $chain;
     }
 
     public function findByRelation(array $params, int $page, int $perPage)
@@ -62,3 +157,4 @@ abstract class AdvancedRepository extends ServiceEntityRepository
         return $paginator;
     }
 }
+
